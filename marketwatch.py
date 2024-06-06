9ae75e7b4272f1a358fe1dfda128e463b2c475db
Rafael Kisters Ventura da Silva <kistters@gmail.com>
2024-06-06 00:21:10 -0300

    MarketWatch crawler using selenium grid - CAPTCHA blocks

---
+++ marketwatch.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+
+driver = webdriver.Remote(
+    command_executor="http://127.0.0.1:4444",
+    options=options
+)
+
+driver.get('https://www.marketwatch.com/investing/stock/aapl')
+
+screenshot_path = '01-marketwatch-aapl-screenshot.png'
+driver.save_screenshot(screenshot_path)
+with open('01-marketwatch.aapl.selenium.html', 'w', encoding='utf-8') as file:
+    file.write(driver.page_source)
+
+driver.quit()
